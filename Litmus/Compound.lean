6e9a37e32b209ad5c436ea89860e2ce7fd46bd43
Andres Goens <andres.goens@ed.ac.uk>
2022-10-25 16:42:19 +0100

    Removed (axiomatic) annotations from litmus tests in CMM

--- Litmus/Compound.lean
+++ Litmus/Compound.lean
@@ -3,10 +3,12 @@ import Litmus.CompoundTraces
 namespace Litmus
 
 deflitmus IRIW_tso := {| W x=1 ||  R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 |}
-  where sys := { {T0, T1, T2, T3}. x86} 𐄂
+  where sys := { {T0, T1, T2, T3}. x86}
+
 deflitmus IRIW_ptx := {| W x=1 ||  R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 |}
-  where sys := { {T0, T1, T2, T3}. ptx} ✓
+  where sys := { {T0, T1, T2, T3}. ptx}
+
 deflitmus IRIW_tso_reads_ptx_writes := {| W x=1 ||  R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 |}
-  where sys := { {T0, T3}. ptx, {T1, T2}. tso} ✓
+  where sys := { {T0, T3}. ptx, {T1, T2}. tso}
 
 end Litmus

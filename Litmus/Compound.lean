fe9cfd2cff1d8abbac742614806296d72e49e8c2
Andres Goens <andres.goens@ed.ac.uk>
2022-10-28 07:27:00 +0100

    Minor: added some missing commas

--- Litmus/Compound.lean
+++ Litmus/Compound.lean
@@ -12,10 +12,10 @@ deflitmus IRIW_tso_reads_ptx_writes := {| W x=1 ||  R x // 1 ; R y // 0 || R y /
   where sys := { {T0, T3}. ptx, {T1, T2}. tso}
 
 deflitmus IRIW_ptx_reads_tso_writes := {| W x=1 ||  R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 |}
-  where sys := { {T0, T3}. tso, {T1}. ptx {T2}. ptx}
+  where sys := { {T0, T3}. tso, {T1}. ptx, {T2}. ptx}
 
 deflitmus IRIW_tso_one_read_ptx_writes := {| W x=1 ||  R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 |}
-  where sys := { {T0}. ptx {T1}. ptx {T3}. ptx, {T2}. tso}
+  where sys := { {T0}. ptx, {T1}. ptx, {T3}. ptx, {T2}. tso}
 
 deflitmus IRIW_tso_fences := {| W x=1 ||  R x // 1 ; Fence; R y // 0 || R y // 1; Fence; R x // 0 || W y=1 |}
   where sys := { {T0, T1, T2, T3}. tso}

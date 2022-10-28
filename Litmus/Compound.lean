0cf8fb29eee98ab381144fe1e4167d09c9eb850f
Susmit Sarkar <Susmit.Sarkar@st-andrews.ac.uk>
2022-10-28 14:32:13 +0000

    Compound MP tests

--- Litmus/Compound.lean
+++ Litmus/Compound.lean
@@ -26,6 +26,42 @@ deflitmus IRIW_ptx_fences := {| W x=1 ||  R x // 1 ; Fence; R y // 0 || R y // 1
 deflitmus IRIW_ptx_reads_tso_writes_fences := {| W x=1 ||  R x // 1 ; Fence;  R y // 0 || R y // 1; Fence; R x // 0 || W y=1 |}
   where sys := { {T0, T3}. tso, {T1}. ptx, {T2}. tso}
 
+deflitmus MP_tso := {|  W x=1; W y=1 ||  R y // 1; R x // 0 |}
+  where sys := { {T0, T1}. tso}
+
+deflitmus MP_ptx := {|  W x=1; W y=1 ||  R y // 1; R x // 0 |}
+  where sys := { {T0, T1}. ptx}
+
+deflitmus MP_writes_tso := {|  W x=1; W y=1 ||  R y // 1; R x // 0 |}
+  where sys := { {T0}. tso, {T1}. ptx}
+
+deflitmus MP_writes_ptx := {|  W x=1; W y=1 ||  R y // 1; R x // 0 |}
+  where sys := { {T0}. ptx, {T1}. tso}
+
+deflitmus MP_writes_tso_ptx_acq_sys := {|  W x=1; W y=1 ||  R. sys_acq y // 1; R x // 0 |}
+  where sys := { {T0}. tso, {T1}. ptx}
+
+deflitmus MP_writes_tso_ptx_acq_cta := {|  W x=1; W y=1 ||  R. cta_acq y // 1; R x // 0 |}
+  where sys := { {T0}. tso, {T1}. ptx}
+
+deflitmus MP_writes_tso_ptx_acq_sys_fence := {|  W x=1; W y=1 ||  R y // 1; Fence. sys_acq; R x // 0 |}
+  where sys := { {T0}. tso, {T1}. ptx}
+
+deflitmus MP_writes_tso_ptx_acq_cta_fence := {|  W x=1; W y=1 ||  R y // 1; Fence. cta_acq; R x // 0 |}
+  where sys := { {T0}. tso, {T1}. ptx}
+
+deflitmus MP_writes_ptx_rel_sys := {|  W x=1; W. sys_rel y=1 ||  R y // 1; R x // 0 |}
+  where sys := { {T0}. ptx, {T1}. tso}
+
+deflitmus MP_writes_ptx_rel_cta := {|  W x=1; W. cta_rel y=1 ||  R y // 1; R x // 0 |}
+  where sys := { {T0}. ptx, {T1}. tso}
+
+deflitmus MP_writes_ptx_rel_sys_fence := {|  W x=1; Fence. sys_rel; W y=1 ||  R y // 1; R x // 0 |}
+  where sys := { {T0}. ptx, {T1}. tso}
+
+deflitmus MP_writes_ptx_rel_cta_fence := {|  W x=1; Fence. cta_rel; W y=1 ||  R y // 1; R x // 0 |}
+  where sys := { {T0}. ptx, {T1}. tso}
+
 def allCompound : List Litmus.Test := litmusTests!
 def compound_2 := allCompound.filter λ lit => lit.numThreads == 2
 def compound_3 := allCompound.filter λ lit => lit.numThreads == 3

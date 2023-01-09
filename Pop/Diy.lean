d123b65f6d7db09ffb9b288cb1a09c8ed30b5dc5
Andres Goens <andres.goens@ed.ac.uk>
2023-01-09 17:51:01 +0000

    Fix preprocessor, add rudimentary parsing function

--- Pop/Diy.lean
+++ Pop/Diy.lean
@@ -29,6 +29,7 @@ syntax result_term "̈̈∨" result_term : result_term
 syntax "(" result_term ")" : result_term
 
 -- Example: SB.litmus
+/-
 syntax "[diy|" test "]" : term
 def SB := [diy|
 X86 SB
@@ -39,3 +40,44 @@ X86 SB
  MOV EAX,[x] | MOV EAX,[y] ; --#(b)Rx0  | (d)Ry0
 exists (0:EAX=0 ∧ 1:EAX=0)
 ]
+-/
+inductive TokenizerState
+  | normal
+  | string
+  | curlyBrackets
+
+def removeCommentsLineCharListAux : List Char → TokenizerState → List Char
+  | c::cs, .normal => match c with
+      | '\"' => c::(removeCommentsLineCharListAux cs .string)
+      | '{'  => c::(removeCommentsLineCharListAux cs .curlyBrackets)
+      | ';'  => c::[]
+      | _    => c::(removeCommentsLineCharListAux cs .normal)
+  | c::cs, .string => match c with
+      | '\"' => c::(removeCommentsLineCharListAux cs .normal)
+      | _    => c::(removeCommentsLineCharListAux cs .string)
+  | c::cs, .curlyBrackets => match c with
+      | '}' => c::(removeCommentsLineCharListAux cs .normal)
+      | _    => c::(removeCommentsLineCharListAux cs .curlyBrackets)
+  | l, _ => l
+
+def removeCommentsLineCharList : List Char → List Char
+  | l => removeCommentsLineCharListAux l .normal
+
+def replaceAnds : List Char → List Char
+  | c1::c2::cs => if c1 == '/' && c2 == '\\' then '∧'::cs else c1::(replaceAnds (c2::cs))
+  | l => l
+
+def preprocessLine : String → String :=
+λ s => { data := replaceAnds (removeCommentsLineCharList s.data) }
+
+def preprocess (input : String) : String :=
+  let lines := input.splitOn "\n"
+  String.intercalate "\n" $ lines.map preprocessLine
+
+def parse (input : String) : IO Lean.Syntax := do
+  let env ← Lean.importModules [{ module := `Pop.Diy }] {}
+  let preprocessed := preprocess input
+  match Lean.Parser.runParserCategory env `test preprocessed with
+    | .error msg => IO.println s!"Error: {msg}"; return default
+    | .ok stx => return stx
+

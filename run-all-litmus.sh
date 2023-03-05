859f036449baa31fd325e34a55e9c6966015bff4
Andres Goens <andres.goens@ed.ac.uk>
2023-03-05 16:56:43 +0000

    Unified operational + axiomatic in run-all script

--- run-all-litmus.sh
+++ run-all-litmus.sh
@@ -10,32 +10,19 @@
 # the exploration is not exhaustive.
 NUM_ITERATIONS=10000 #100000
 ARCHITECTURES=TSO,PTX,Compound,XC,XCTSO
-ARCHITECTURES_ALLOY=TSO,PTX,Compound
+
 
 PROJECT_ROOT=`pwd`
 AXIOMATIC_DIR=$PROJECT_ROOT/cmm-axiomatic
 ALLOY_JAR=$PROJECT_ROOT/org.alloytools.alloy/org.alloytools.alloy.cli/target/org.alloytools.alloy.cli.jar
-OUTPUT=axiomatic_results.csv
-OUTPUT_OPERATIONAL=operational_results.csv
+OUTPUT=results.csv
+
+echo "=============================================="
+echo "          Building Tools"
+echo "=============================================="
 
 # Build binary in case it's not already built
 lake update && lake build
-
-echo "=============================================="
-echo "          Running Operational Model"
-echo "=============================================="
-
-# Cleanup
-rm $OUTPUT_OPERATIONAL
-for ARCH in `echo $ARCHITECTURES | sed 's/,/ /g'`; do
-    ./build/bin/pop -e -a $ARCH -i $NUM_ITERATIONS | tee -a $OUTPUT_OPERATIONAL
-done
-
-
-echo "=============================================="
-echo "          Running Alloy Axiomatic"
-echo "=============================================="
-
 # Build Alloy
 cd org.alloytools.alloy
 ./gradlew build
@@ -44,19 +31,31 @@ cd $PROJECT_ROOT
 #cleanup
 rm -rf litmus_alloy*
 
-echo "architecture,test,expected,result" > $OUTPUT
-for ARCH in `echo $ARCHITECTURES_ALLOY | sed 's/,/ /g'`; do
+echo "=============================================="
+echo "          Running Litmus Tests"
+echo "=============================================="
+
+
+echo "architecture,test,operational,axiomatic" > $OUTPUT
+for ARCH in `echo $ARCHITECTURES | sed 's/,/ /g'`; do
    #Build Axiomatic Alloy litmus tests
+  # We only have axiomatic for TSO,PTX,Compound
   DIR=litmus_alloy_$ARCH
   ./build/bin/pop -a $ARCH -A -D $DIR
   TESTS=`ls $DIR/*.als`
   cp $AXIOMATIC_DIR/*.als $DIR
-  for TEST in $TESTS; do
+  for TESTFILE in $TESTS; do
+    TEST=`basename $TESTFILE .als`
+     echo "Running Litmus Test $ARCH/${TEST}"
      echo -n $ARCH, >> $OUTPUT
-     echo -n ${TEST##*/}, >> $OUTPUT
-     grep "// Expected:" $TEST | grep -o -e "?" -e "𐄂" -e "✓" | tr -d '\n' >> $OUTPUT
-     echo -n "," >> $OUTPUT
-     java -jar $ALLOY_JAR exec $TEST | grep -o -e "INSTANCE" -e "UNSAT" | sed -e 's/INSTANCE/✓/g' -e 's/UNSAT/𐄂/g'  >> $OUTPUT 2>> litmus_alloy.log
+     echo -n $TEST, >> $OUTPUT
+    ./build/bin/pop -e -a $ARCH -i $NUM_ITERATIONS -l $TEST | grep -o -e "𐄂" -e "✓" -e "𐄂?" | tr '\n' ',' | cut -d ',' -f 2 | tr '\n' ',' | tee -a $OUTPUT
+     #echo -n "," >> $OUTPUT
+    if [ "$ARCH" = "TSO" ] || [ "$ARCH" = "PTX" ] || [ "$ARCH" = "Compound" ]; then
+      java -jar $ALLOY_JAR exec $TESTFILE 2>> litmus_alloy.log | grep -o -e "INSTANCE" -e "UNSAT" | sed -e 's/INSTANCE/✓/g' -e 's/UNSAT/𐄂/g' | tee -a $OUTPUT
+    else
+      echo "?" >> $OUTPUT
+    fi
   done;
 done
 cat $OUTPUT

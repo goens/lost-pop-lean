993600534050f2e6a48fc928932db9edb0427bd3
Andres Goens <andres.goens@ed.ac.uk>
2023-03-05 18:26:14 +0000

    Cleanup analysis output

--- analysis.R
+++ analysis.R
@@ -6,8 +6,8 @@ results <- read_csv("results.csv",show_col_types = FALSE)
 #Check for NAs
 nas <- results %>% 
   summarise(across(everything(), ~ sum(is.na(.))))
-print("missing values")
-print(nas)
+missing <- sum(nas$operational) + sum(nas$axiomatic)
+print(paste(missing, "missing values"))
 
 assume_timeouts <- results %>%
   #Filter out where we don't have axiomatic models (XC/TSOXC)
@@ -16,8 +16,7 @@ assume_timeouts <- results %>%
           architecture == "Compound") %>%
   mutate(operational = ifelse(operational == "𐄂?", "𐄂",operational))
   
-print("discrepancies found")
 discrepancies <- assume_timeouts %>%
   filter(axiomatic != operational)
+print("following discrepancies found:")
 print(discrepancies)
-

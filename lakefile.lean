98d214cd242b5dda3aa5f73c199a91cf7508f246
Andrés Goens <andres@goens.org>
2024-04-16 19:43:31 +0200

    bump lean version

--- lakefile.lean
+++ lakefile.lean
@@ -20,6 +20,6 @@ lean_lib Litmus {
 lean_exe pop {
   root := `Main
 }
-require std from git "https://github.com/leanprover/std4.git" @ "0f6bc5b"
+require std from git "https://github.com/leanprover/std4.git" @ "v4.7.0"
 require Cli from git "https://github.com/mhuisi/lean4-cli.git" @ "v2.2.0-lv4.0.0"
 require Murphi from git "https://github.com/goens/lean-murphi.git" @ "1a5794d23923fb7fd57cc45cfd44c6da3588f31b"

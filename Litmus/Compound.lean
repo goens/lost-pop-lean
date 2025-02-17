5162d432fd02f64c6757706e2ac6a865663bc62d
Andrés Goens <andres@goens.org>
2025-02-17 15:37:07 +0100

    add Vijay's new litmus tests for trailing mappings

--- Litmus/Compound.lean
+++ Litmus/Compound.lean
@@ -6,212 +6,220 @@ import Litmus.CompoundTraces
 namespace Compound
 namespace Litmus
 
-deflitmus IRIW_tso := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 
- where sys := { {T0, T1, T2, T3}. x86} 
+deflitmus IRIW_tso := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1
+ where sys := { {T0, T1, T2, T3}. x86}
  expect 𐄂
 
-deflitmus IRIW_ptx := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 
- where sys := { {T0, T1, T2, T3}. PTX} 
+deflitmus IRIW_ptx := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1
+ where sys := { {T0, T1, T2, T3}. PTX}
  expect ✓
 
-deflitmus IRIW_tso_reads_ptx_writes := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 
- where sys := { {T0, T3}. PTX, {T1, T2}. x86} 
+deflitmus IRIW_tso_reads_ptx_writes := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1
+ where sys := { {T0, T3}. PTX, {T1, T2}. x86}
  expect 𐄂 -- expect ✓ with change
 
-deflitmus IRIW_ptx_reads_tso_writes := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 
- where sys := { {T0, T3}. x86, {T1}. PTX, {T2}. PTX} 
+deflitmus IRIW_ptx_reads_tso_writes := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1
+ where sys := { {T0, T3}. x86, {T1}. PTX, {T2}. PTX}
  expect ✓
 
-deflitmus IRIW_tso_one_read_ptx_writes := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 
- where sys := { {T0}. PTX, {T1}. PTX, {T3}. PTX, {T2}. x86} 
+deflitmus IRIW_tso_one_read_ptx_writes := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1
+ where sys := { {T0}. PTX, {T1}. PTX, {T3}. PTX, {T2}. x86}
  expect ✓
 
-deflitmus IRIW_tso_fences := W x=1 || R x // 1 ; Fence; R y // 0 || R y // 1; Fence; R x // 0 || W y=1 
- where sys := { {T0, T1, T2, T3}. x86} 
+deflitmus IRIW_tso_fences := W x=1 || R x // 1 ; Fence; R y // 0 || R y // 1; Fence; R x // 0 || W y=1
+ where sys := { {T0, T1, T2, T3}. x86}
  expect 𐄂
 
-deflitmus IRIW_ptx_fences := W x=1 || R x // 1 ; Fence; R y // 0 || R y // 1; Fence; R x // 0 || W y=1 
- where sys := { {T0, T1, T2, T3}. PTX} 
+deflitmus IRIW_ptx_fences := W x=1 || R x // 1 ; Fence; R y // 0 || R y // 1; Fence; R x // 0 || W y=1
+ where sys := { {T0, T1, T2, T3}. PTX}
  expect 𐄂
 
-deflitmus IRIW_ptx_reads_tso_writes_fences := W x=1 || R x // 1 ; Fence; R y // 0 || R y // 1; Fence; R x // 0 || W y=1 
- where sys := { {T0, T3}. x86, {T1}. PTX, {T2}. x86} 
+deflitmus IRIW_ptx_reads_tso_writes_fences := W x=1 || R x // 1 ; Fence; R y // 0 || R y // 1; Fence; R x // 0 || W y=1
+ where sys := { {T0, T3}. x86, {T1}. PTX, {T2}. x86}
  expect 𐄂
 
-deflitmus IRIW_tso_left_ptx_right := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1 
- where sys := { {T0, T1}. x86, {T2, T3}. PTX} 
+deflitmus IRIW_tso_left_ptx_right := W x=1 || R x // 1 ; R y // 0 || R y // 1; R x // 0 || W y=1
+ where sys := { {T0, T1}. x86, {T2, T3}. PTX}
  expect ✓
 
-deflitmus IRIW_tso_left_ptx_right_fence := W x=1 || R x // 1 ; R y // 0 || R y // 1; Fence; R x // 0 || W y=1 
- where sys := { {T0, T1}. x86, {T2, T3}. PTX} 
+deflitmus IRIW_tso_left_ptx_right_fence := W x=1 || R x // 1 ; R y // 0 || R y // 1; Fence; R x // 0 || W y=1
+ where sys := { {T0, T1}. x86, {T2, T3}. PTX}
  expect 𐄂
 
-deflitmus IRIW_tso_left_ptx_right_fenceacqrel := W x=1 || R x // 1 ; R y // 0 || R y // 1; Fence.sys_acqrel; R x // 0 || W y=1 
- where sys := { {T0, T1}. x86, {T2, T3}. PTX} 
+deflitmus IRIW_tso_left_ptx_right_fenceacqrel := W x=1 || R x // 1 ; R y // 0 || R y // 1; Fence.sys_acqrel; R x // 0 || W y=1
+ where sys := { {T0, T1}. x86, {T2, T3}. PTX}
  expect ✓
 
-deflitmus IRIW_tso_fence_left_ptx_right_fenceacqrel := W x=1 || R x // 1 ; Fence; R y // 0 || R y // 1; Fence.sys_acqrel; R x // 0 || W y=1 
- where sys := { {T0, T1}. x86, {T2, T3}. PTX} 
+deflitmus IRIW_tso_fence_left_ptx_right_fenceacqrel := W x=1 || R x // 1 ; Fence; R y // 0 || R y // 1; Fence.sys_acqrel; R x // 0 || W y=1
+ where sys := { {T0, T1}. x86, {T2, T3}. PTX}
  expect ✓
 
-deflitmus IRIW_one_tso_read_fence_rest_ptx := W.sys_rel x=1 || R x // 1; R y // 0 || R.sys_acq y // 1; Fence.sys_sc; R.sys_acq x // 0 || W.sys_rel y=1 
- where sys := { {T0, T2, T3}. PTX, {T1}. x86} 
+deflitmus IRIW_one_tso_read_fence_rest_ptx := W.sys_rel x=1 || R x // 1; R y // 0 || R.sys_acq y // 1; Fence.sys_sc; R.sys_acq x // 0 || W.sys_rel y=1
+ where sys := { {T0, T2, T3}. PTX, {T1}. x86}
  expect 𐄂
 
-deflitmus IRIW_one_tso_read_fence_rest_ptx_rlx := W x=1 || R x // 1; R y // 0 || R y // 1; Fence.sys_sc; R x // 0 || W y=1 
- where sys := { {T0, T2, T3}. PTX, {T1}. x86} 
+deflitmus IRIW_one_tso_read_fence_rest_ptx_rlx := W x=1 || R x // 1; R y // 0 || R y // 1; Fence.sys_sc; R x // 0 || W y=1
+ where sys := { {T0, T2, T3}. PTX, {T1}. x86}
  expect 𐄂
 
-deflitmus MP_tso :=  W x=1; W y=1 || R y // 1; R x // 0 
- where sys := { {T0, T1}. x86} 
+deflitmus MP_tso :=  W x=1; W y=1 || R y // 1; R x // 0
+ where sys := { {T0, T1}. x86}
  expect 𐄂
 
-deflitmus MP_ptx :=  W x=1; W y=1 || R y // 1; R x // 0 
- where sys := { {T0, T1}. PTX} 
+deflitmus MP_ptx :=  W x=1; W y=1 || R y // 1; R x // 0
+ where sys := { {T0, T1}. PTX}
  expect ✓
 
-deflitmus MP_writes_tso :=  W x=1; W y=1 || R y // 1; R x // 0 
- where sys := { {T0}. x86, {T1}. PTX} 
+deflitmus MP_writes_tso :=  W x=1; W y=1 || R y // 1; R x // 0
+ where sys := { {T0}. x86, {T1}. PTX}
  expect ✓
 
-deflitmus MP_writes_ptx :=  W x=1; W y=1 || R y // 1; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus MP_writes_ptx :=  W x=1; W y=1 || R y // 1; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect ✓
 
-deflitmus MP_writes_tso_ptx_acq_sys :=  W x=1; W y=1 || R.sys_acq y // 1; R x // 0 
- where sys := { {T0}. x86, {T1}. PTX} 
+deflitmus MP_writes_tso_ptx_acq_sys :=  W x=1; W y=1 || R.sys_acq y // 1; R x // 0
+ where sys := { {T0}. x86, {T1}. PTX}
  expect 𐄂
 
-deflitmus MP_writes_tso_ptx_acq_cta :=  W x=1; W y=1 || R.cta_acq y // 1; R x // 0 
- where sys := { {T0}. x86, {T1}. PTX} 
+deflitmus MP_writes_tso_ptx_acq_cta :=  W x=1; W y=1 || R.cta_acq y // 1; R x // 0
+ where sys := { {T0}. x86, {T1}. PTX}
  expect ✓
 
-deflitmus MP_writes_tso_ptx_acq_sys_fence :=  W x=1; W y=1 || R y // 1; Fence.sys_acq; R x // 0 
- where sys := { {T0}. x86, {T1}. PTX} 
+deflitmus MP_writes_tso_ptx_acq_sys_fence :=  W x=1; W y=1 || R y // 1; Fence.sys_acq; R x // 0
+ where sys := { {T0}. x86, {T1}. PTX}
  expect 𐄂
 
-deflitmus MP_writes_tso_ptx_acq_cta_fence :=  W x=1; W y=1 || R y // 1; Fence.cta_acq; R x // 0 
- where sys := { {T0}. x86, {T1}. PTX} 
+deflitmus MP_writes_tso_ptx_acq_cta_fence :=  W x=1; W y=1 || R y // 1; Fence.cta_acq; R x // 0
+ where sys := { {T0}. x86, {T1}. PTX}
  expect ✓
 
-deflitmus MP_writes_ptx_rel_sys :=  W x=1; W.sys_rel y=1 || R y // 1; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus MP_writes_ptx_rel_sys :=  W x=1; W.sys_rel y=1 || R y // 1; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect 𐄂
 
-deflitmus MP_writes_ptx_rel_cta :=  W x=1; W.cta_rel y=1 || R y // 1; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus MP_writes_ptx_rel_cta :=  W x=1; W.cta_rel y=1 || R y // 1; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect ✓
 
-deflitmus MP_writes_ptx_rel_sys_fence :=  W x=1; Fence.sys_rel; W y=1 || R y // 1; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus MP_writes_ptx_rel_sys_fence :=  W x=1; Fence.sys_rel; W y=1 || R y // 1; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect 𐄂
 
-deflitmus MP_writes_ptx_rel_cta_fence :=  W x=1; Fence.cta_rel; W y=1 || R y // 1; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus MP_writes_ptx_rel_cta_fence :=  W x=1; Fence.cta_rel; W y=1 || R y // 1; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect ✓
 
 deflitmus WRC_ptx_reader_dep := W x=1 || R x // 1; W y = 1 || R y // 1 ;dep R x // 0
- where sys := { {T0, T1}. x86, {T2}. PTX} 
+ where sys := { {T0, T1}. x86, {T2}. PTX}
  expect ✓ -- TODO: This would probably be disallowed in the operational model (it is)
 
 deflitmus WRC_ptx_reader_acq := W x=1 || R x // 1; W y = 1 || R.sys_acq y // 1 ; R x // 0
- where sys := { {T0, T1}. x86, {T2}. PTX} 
+ where sys := { {T0, T1}. x86, {T2}. PTX}
  expect 𐄂
 
 deflitmus WRC_middle_tso_dep := W x=1 || R x // 1; W y = 1 || R y // 1 ;dep R x // 0
- where sys := { {T1}. x86, {T0, T2}. PTX} 
+ where sys := { {T1}. x86, {T0, T2}. PTX}
  expect ✓ -- TODO: This would probably be disallowed in the operational model (it is)
 
 deflitmus WRC_middle_tso_acq := W x=1 || R x // 1; W y = 1 || R.sys_acq y // 1 ;R x // 0
- where sys := { {T1}. x86, {T0, T2}. PTX} 
+ where sys := { {T1}. x86, {T0, T2}. PTX}
  expect 𐄂
 
 deflitmus WRC_middle_ptx_acqrel := W x=1 || R.sys_acq x // 1; W.sys_rel y = 1 || R y // 1 ;R x // 0
- where sys := { {T1}. PTX, {T0, T2}. x86} 
+ where sys := { {T1}. PTX, {T0, T2}. x86}
  expect 𐄂
 
 deflitmus WRC_middle_ptx_acqrel_cta := W x=1 || R.cta_acq x // 1; W.cta_rel y = 1 || R y // 1 ;R x // 0
- where sys := { {T1}. PTX, {T0, T2}. x86} 
+ where sys := { {T1}. PTX, {T0, T2}. x86}
  expect ✓
 
 deflitmus WRC_middle_ptx_acqrel_fence := W x=1 || R x // 1; Fence.sys_acqrel; W y = 1 || R y // 1 ;R x // 0
- where sys := { {T1}. PTX, {T0, T2}. x86} 
+ where sys := { {T1}. PTX, {T0, T2}. x86}
  expect 𐄂
 
 deflitmus WRC_middle_ptx_acqrel_cta_fence := W x=1 || R x // 1; Fence.cta_acqrel; W y = 1 || R y // 1 ;R x // 0
- where sys := { {T1}. PTX, {T0, T2}. x86} 
+ where sys := { {T1}. PTX, {T0, T2}. x86}
  expect ✓
 
 deflitmus ISA2_right_ptx := W x=1; W y=1 || R y // 1; W z = 1 || R z // 1 ; R x // 0
- where sys := { {T0, T1}. x86, {T2}. PTX} 
+ where sys := { {T0, T1}. x86, {T2}. PTX}
  expect ✓
 
 deflitmus ISA2_right_ptx_acq := W x=1; W y=1 || R y // 1; W z = 1 || R.sys_acq z // 1 ; R x // 0
- where sys := { {T0, T1}. x86, {T2}. PTX} 
+ where sys := { {T0, T1}. x86, {T2}. PTX}
  expect 𐄂
 
 deflitmus ISA2_right_ptx_acq_cta := W x=1; W y=1 || R y // 1; W z = 1 || R.cta_acq z // 1 ; R x // 0
- where sys := { {T0, T1}. x86, {T2}. PTX} 
+ where sys := { {T0, T1}. x86, {T2}. PTX}
  expect ✓
 
 deflitmus ISA2_middle_ptx := W x=1; W y=1 || R y // 1; W z = 1 || R z // 1 ; R x // 0
- where sys := { {T0, T2}. x86, {T1}. PTX} 
+ where sys := { {T0, T2}. x86, {T1}. PTX}
  expect ✓
 
 deflitmus ISA2_middle_ptx_acqrel := W x=1; W y=1 || R.sys_acq y // 1; W.sys_rel z = 1 || R z // 1 ; R x // 0
- where sys := { {T0, T2}. x86, {T1}. PTX} 
+ where sys := { {T0, T2}. x86, {T1}. PTX}
  expect 𐄂
 
 deflitmus ISA2_middle_ptx_acqrel_fence := W x=1; W y=1 || R y // 1; Fence.sys_acqrel; W z = 1 || R z // 1 ; R x // 0
- where sys := { {T0, T2}. x86, {T1}. PTX} 
+ where sys := { {T0, T2}. x86, {T1}. PTX}
  expect 𐄂
 
 deflitmus ISA2_middle_ptx_acq_fence := W x=1; W y=1 || R y // 1; Fence.sys_acq; W z = 1 || R z // 1 ; R x // 0
- where sys := { {T0, T2}. x86, {T1}. PTX} 
+ where sys := { {T0, T2}. x86, {T1}. PTX}
  expect 𐄂 -- TODO: discuss this one with others; I think this should be allowed axiomatically too.
 
 deflitmus ISA2_middle_ptx_rel_fence := W x=1; W y=1 || R y // 1; Fence.sys_rel; W z = 1 || R z // 1 ; R x // 0
- where sys := { {T0, T2}. x86, {T1}. PTX} 
+ where sys := { {T0, T2}. x86, {T1}. PTX}
  expect ✓
 
-deflitmus dekkers_tso := W x=1; R y //0 || W y=1; R x // 0 
- where sys := { {T0, T1}. x86} 
+deflitmus dekkers_tso := W x=1; R y //0 || W y=1; R x // 0
+ where sys := { {T0, T1}. x86}
  expect ✓
 
-deflitmus dekkers_tso_ptx_dont_care := W x=1; R y //0 || W y=1; R x // 0 || Fence.sys_sc 
- where sys := { {T0, T1}. x86, {T2}. PTX} 
+deflitmus dekkers_tso_ptx_dont_care := W x=1; R y //0 || W y=1; R x // 0 || Fence.sys_sc
+ where sys := { {T0, T1}. x86, {T2}. PTX}
  expect ✓
 
-deflitmus dekkers_tso_fence := W x=1; Fence; R y //0 || W y=1; Fence; R x // 0 
- where sys := { {T0, T1}. x86} 
+deflitmus dekkers_tso_fence := W x=1; Fence; R y //0 || W y=1; Fence; R x // 0
+ where sys := { {T0, T1}. x86}
  expect 𐄂
 
-deflitmus dekkers_ptx := W x=1; R y //0 || W y=1; R x // 0 
- where sys := { {T0, T1}. PTX} 
+deflitmus dekkers_ptx := W x=1; R y //0 || W y=1; R x // 0
+ where sys := { {T0, T1}. PTX}
  expect ✓
 
-deflitmus dekkers_ptx_fence := W x=1; Fence; R y //0 || W y=1; Fence; R x // 0 
- where sys := { {T0, T1}. PTX} 
+deflitmus dekkers_ptx_fence := W x=1; Fence; R y //0 || W y=1; Fence; R x // 0
+ where sys := { {T0, T1}. PTX}
  expect 𐄂
 
-deflitmus dekkers_mix := W x=1; R y //0 || W y=1; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus dekkers_mix := W x=1; R y //0 || W y=1; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect ✓
 
-deflitmus dekkers_mix_fence := W x=1; Fence; R y //0 || W y=1; Fence; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus dekkers_mix_fence := W x=1; Fence; R y //0 || W y=1; Fence; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect 𐄂
 
-deflitmus dekkers_mix_only_tso_fence := W x=1; R y //0 || W y=1; Fence; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus dekkers_mix_only_tso_fence := W x=1; R y //0 || W y=1; Fence; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect ✓
 
-deflitmus dekkers_mix_only_ptx_fence := W x=1; Fence; R y //0 || W y=1; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus dekkers_mix_only_ptx_fence := W x=1; Fence; R y //0 || W y=1; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
  expect ✓
 
-deflitmus dekkers_mix_only_cta_fence := W x=1; Fence.cta_sc; R y //0 || W y=1; Fence; R x // 0 
- where sys := { {T0}. PTX, {T1}. x86} 
+deflitmus dekkers_mix_only_cta_fence := W x=1; Fence.cta_sc; R y //0 || W y=1; Fence; R x // 0
+ where sys := { {T0}. PTX, {T1}. x86}
+ expect ✓
+
+deflitmus vijay1 := Fence; W.sys_rel X = 1; W.sys_rel Y = 1 || Fence; W.sys_rel Z = 1; W.sys_rel A = 1 || R Y // 1; R Z // 0 || R A // 1; R X // 0
+ where sys := { {T0, T1}. PTX, {T2, T3}. x86}
+ expect ✓
+
+deflitmus vijay2 := Fence; W.sys_rel X = 1; W.sys_rel Y = 1 || Fence; W.sys_rel Z = 2; W.sys_rel A = 1 || R Y // 1; W Z = 1; R Z // 2 || R A // 1; R X // 0
+ where sys := { {T0, T1}. PTX, {T2, T3}. x86}
  expect ✓
 
 def allTests : List Litmus.Test := litmusTests!
--- Litmus/PTX.lean
+++ Litmus/PTX.lean
@@ -14,6 +14,14 @@ deflitmus ISA2_mixed_scoped_cta_last := W x=1; W.cta_rel y=1 || R.cta_acq y // 1
  deflitmus WRC_mixed_cta_2_1_acqrel := W.cta_rlx x=1 || R.cta_rlx x // 1; Fence.sys_acqrel; W.sys_rlx y = 1 || R.sys_rlx y // 1 ; Fence.sys_acq; R.sys_rlx x // 0
  where sys := { {T0, T1}, {T2}} expect 𐄂
 
+deflitmus new_mixed_scoped_cta_last := W x=1; W.cta_rel y=1 || R.cta_acq y // 1; W.sys_rel z = 1 || R.sys_acq z // 1 ; R.cta_rlx x // 0
+ where sys := { {T0, T1}, {T2}}
+
+-- Disallowed in LOST-POP PTX: the write Y can't propagate until X has propagated globally
+deflitmus vijay1 := W.sys_rel X = 1; W.sys_rel Y = 1 || R.sys_acq Y // 1; R.sys_acq Z // 0 || W.sys_rel Z = 1; W.sys_rel A = 1 || R.sys_acq A // 1; R.sys_acq X // 0
+
+deflitmus vijay2 := W.sys_rel X = 1; W.sys_rel Y = 1 || R.sys_acq Y // 1; W.sys_rel Z = 1; R Z // 2 || W.sys_rel Z = 2; W.sys_rel A = 1 || R.sys_acq A // 1; R.sys_acq X // 0
+
 def allTests : List Litmus.Test := litmusTests!
 
 end Litmus
